5d470f8dcda26ebb22f3ddcdf527c05011446f7a
CreatorLynx <1_linx_1@mail.ru>
2026-07-03 23:40:06 +0300

    Tested controller

--- Balurs-tale-unity/Assets/Controller/PlayerImpact.cs
+++ Balurs-tale-unity/Assets/Controller/PlayerImpact.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerImpactController : MonoBehaviour
+public class PlayerImpact : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
